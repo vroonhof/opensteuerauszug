afa9601789415534d41335b8d827ff6497adf4c7
Sahal Yacoob <sahal.yacoob@cern.ch>
2026-04-13 07:55:36 +0200

    clean setup_pixi.sh

--- scripts/setup_pixi.sh
+++ scripts/setup_pixi.sh
@@ -10,27 +10,8 @@ fi
 
 # check if pixi is already installed
 if [[ "$PIXI_INSTALLED" = false ]]; then
-  printf "No installed pixi found!\n"
-  printf -- "--> Installing pixi ..\n\n"
-  # installation for macOS or linux 
-  if [[ $OSTYPE == 'darwin'* || $OSTYPE == 'linux'* ]]; then
-    curl -fsSL https://pixi.sh/install.sh | sh
-    if [ -n "$BASH_VERSION" ]; then
-      printf -- "--> sourcing ~/.bashrc\n\n"
-      source ~/.bashrc
-    elif [ -n "$ZSH_VERSION" ]; then
-      printf -- "--> sourcing ~/.zshrc\n\n"
-      source ~/.zshrc
-    elif [ -n "$tcsh" ] || [ -n "$csh" ]; then
-      printf -- "--> sourcing ~/.cshrc\n\n"
-      source ~/.cshrc
-    fi
-  # installation for linux
-  # other operating system not supported
-  else
     echo "Operating system not supported. Setup not possible."
     return 1 # exit will kill bash after sourcing, use return
-  fi
 else
   printf -- "--> pixi found.\n\n"
 fi
@@ -41,9 +22,7 @@ if ! pixi info | grep -q "Name: $PIXI_ENV_NAME"; then
   printf -- "--> pixi environment '$PIXI_ENV_NAME' not found\n"
   printf -- "--> Creating new pixi workspace: $PIXI_ENV_NAME\n"
   pixi init
-  printf -- "--> adding framework as pypi package\n"
   pixi add --pypi --editable "$PIXI_ENV_NAME @ file:$PWD"
-  pixi add --pypi "pdf417decoder @ git+https://github.com/vroonhof/pdf417decoder.git#subdirectory=python"
   printf -- "--> framework added\n"
 else
   printf --  "--> pixi workspace $PIXI_ENV_NAME found\n\n"
@@ -51,11 +30,11 @@ fi
 # activate pixi 
 
 if [ $# -eq 0 ]; then 
-  printf -- "--> Activating pixi environment\n"
-  pixi shell
+  printf -- "--> Activating pixi dev environment\n"
+  pixi shell -e dev
 elif [ "$1" == "-b" ]; then
   printf -- "--> Not Activating pixi environment\n"
 else
-  printf -- "--> Activating pixi environment\n"
-  pixi shell
+  printf -- "--> Activating pixi dev environment\n"
+  pixi shell -e dev
 fi
